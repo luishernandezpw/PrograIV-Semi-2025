4442c1becf02f9cc605e203fa8566640c0757938
Luis Hernandez <sarco_luis@hotmail.com>
2025-01-23 13:53:36 -0600

    formulario basico de alumnos con HTML + Bootstrap + Vue.js

--- main.js
+++ main.js
@@ -3,7 +3,22 @@ const {createApp} = Vue;
 createApp({
     data() {
         return {
-            nombre: ''
+            codigo: '',
+            nombre: '',
+            direccion: '',
+            telefono: '',
+            email: ''
+        }
+    },
+    methods: {
+        guardarAlumno() {
+            console.log( 
+                this.codigo,
+                this.nombre,
+                this.direccion,
+                this.telefono,
+                this.email
+            );
         }
     }
 }).mount('#app');
