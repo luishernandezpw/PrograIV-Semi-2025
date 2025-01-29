4c87fe5fddb356a601367281443b14ead8298dc7
Luis Hernandez <Estudiante@DESKTOP-J6D5H3A>
2025-01-29 11:57:58 -0600

    Terminacion del CRUD de alumnos con localStorage

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const {createApp} = Vue;
 createApp({
     data() {
         return {
+            alumnos: [],
             codigo: '',
             nombre: '',
             direccion: '',
@@ -11,14 +12,40 @@ createApp({
         }
     },
     methods: {
+        eliminarAlumno(alumno) {
+            if (confirm(`¿Esta seguro de eliminar el alumno ${alumno.nombre}?`)){
+                localStorage.removeItem(alumno.codigo);
+                this.listarAlumnos();
+            }
+        },
+        verAlumno(alumno) {
+            this.codigo = alumno.codigo;
+            this.nombre = alumno.nombre;
+            this.direccion = alumno.direccion;
+            this.telefono = alumno.telefono;
+            this.email = alumno.email;
+        },
         guardarAlumno() {
-            console.log( 
-                this.codigo,
-                this.nombre,
-                this.direccion,
-                this.telefono,
-                this.email
-            );
+            let alumno = {
+                codigo: this.codigo,
+                nombre: this.nombre,
+                direccion: this.direccion,
+                telefono: this.telefono,
+                email: this.email
+            };
+            localStorage.setItem(this.codigo, JSON.stringify(alumno));
+            this.listarAlumnos();
+        },
+        listarAlumnos() {
+            this.alumnos = [];
+            for (let i = 0; i < localStorage.length; i++) {
+                let clave = localStorage.key(i),
+                    valor = localStorage.getItem(clave);
+                this.alumnos.push(JSON.parse(valor));
+            }
         }
+    },
+    created() {
+        this.listarAlumnos();
     }
 }).mount('#app');
