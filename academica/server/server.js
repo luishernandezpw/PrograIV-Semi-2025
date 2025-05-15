27509af730bdcc8efc679b92387223bb72f9c95f
Luis Hernandez <sarco_luis@hotmail.com>
2025-05-15 14:09:04 -0600

    CRUD de amigos con Node.js + MongoDB + Express.js + Alertify.js

--- academica/server/server.js
+++ academica/server/server.js
@@ -1,8 +1,33 @@
-const http = require('http');
+const express = require('express'),
+    app = express(),
+    port = 3000,
+    {MongoClient} = require('mongodb'),
+    url = 'mongodb://localhost:27017',
+    client = new MongoClient(url),
+    dbName = 'db_amigos';
 
-const server = http.createServer((request, response) => {
-        response.writeHead(200, { 'Content-Type': 'text/plain' });
-        response.end('Hola Mundo\n');
-    });
-server.listen(3000);
-console.log('Server running at http://127.0.0.1:3000/');
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+app.post('/api/amigos', async (req, res) => {
+    let amigo = req.body, 
+        db = await conectarMongoDb(),
+        collection = db.collection('amigos');
+    collection.insertOne(amigo);
+    res.send({msg:'ok'});
+});
+app.get('/api/amigos', async (req, res) => {
+    let db = await conectarMongoDb(),
+        collection = db.collection('amigos');
+    let amigos = await collection.find().toArray();
+    res.send(amigos);
+});
+app.listen(port, () => {
+    console.log('Servidor corriendo en el puerto ' + port);
+});
+async function conectarMongoDb(){
+    await client.connect();
+    return client.db(dbName);
+}
