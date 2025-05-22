c0eb7be0c8df8bdf2366f731add294ca5c0c64cd
Luis Hernandez <sarco_luis@hotmail.com>
2025-05-22 13:57:52 -0600

    Terminacion del CRUD de amigos con HTML5 + Bootstrap + Nodejs + MongoDB + Express

--- academica/server/server.js
+++ academica/server/server.js
@@ -1,7 +1,7 @@
 const express = require('express'),
     app = express(),
     port = 3000,
-    {MongoClient} = require('mongodb'),
+    {MongoClient, ObjectId} = require('mongodb'),
     url = 'mongodb://localhost:27017',
     client = new MongoClient(url),
     dbName = 'db_amigos';
@@ -14,15 +14,36 @@ app.get('/', (req, res) => {
 app.post('/api/amigos', async (req, res) => {
     let amigo = req.body, 
         db = await conectarMongoDb(),
-        collection = db.collection('amigos');
-    collection.insertOne(amigo);
-    res.send({msg:'ok'});
+        collection = db.collection('amigos'),
+        result = await collection.insertOne(amigo);
+    res.send({msg:result});
 });
-app.get('/api/amigos', async (req, res) => {
-    let db = await conectarMongoDb(),
-        collection = db.collection('amigos');
-    let amigos = await collection.find().toArray();
+app.put('/api/amigos/', async (req, res) => {
+    let amigo = req.body,
+        db = await conectarMongoDb(),
+        collection = db.collection('amigos'),
+        result = await collection.updateOne({ _id: new ObjectId(amigo.id) }, { $set: amigo });
+    res.send({msg:result});
+});
+app.get('/api/amigos/', async (req, res) => {
+    let buscar = req.query.buscar,
+        db = await conectarMongoDb(),
+        collection = db.collection('amigos'),
+        amigos = await collection.find({
+            $or: [
+                { nombre: { $regex: new RegExp(buscar, 'i') } }, 
+                { telefono: { $regex: new RegExp(buscar, 'i') } }, 
+                { email: { $regex: new RegExp(buscar, 'i') } }
+            ]
+        }).limit(5).toArray();
     res.send(amigos);
+});
+app.delete('/api/amigos/:id', async (req, res) => {
+    let id = req.params.id, 
+        db = await conectarMongoDb(),
+        collection = db.collection('amigos'),
+        result = await collection.deleteOne({ _id: new ObjectId(id) });
+    res.send({msg:result});
 });
 app.listen(port, () => {
     console.log('Servidor corriendo en el puerto ' + port);
