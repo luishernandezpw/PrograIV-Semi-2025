2af73abb79281209bd9395bd36497db779819889
Luis Hernandez <sarco_luis@hotmail.com>
2025-05-29 14:06:35 -0600

    terminacion del chat de amigos

--- academica/server/server.js
+++ academica/server/server.js
@@ -6,7 +6,8 @@ const express = require('express'),
     io = require('socket.io')(http, {
         allowEIO3: true,
         cors: {
-            origin: "http://localhost:3000",
+            origin: ["http://localhost:3000", "http://127.0.1:3000/","http://localhost:3000/"],
+            allowedHeaders: ["Content-Type"],
             credential: true
         }
     }),
@@ -14,10 +15,28 @@ const express = require('express'),
     client = new MongoClient(url),
     dbName = 'db_amigos';
 
-app.use(express.json());
+io.on('connection', (socket) => {
+    console.log('Chat conectado');
 
+    socket.on('mensajeAmigo', async (mensaje) => {
+        let db = await conectarMongoDb(),
+            collection = db.collection('chats'),
+            result = collection.insertOne({ mensaje: mensaje, fecha: new Date() });
+
+        //socket.broadcast.emit('mensajeAmigo', mensaje); // Enviar a todos los demás clientes conectados, menos a mi mismo
+        io.emit('mensajeAmigo', mensaje); // Enviar a todos los clientes conectados. Incluso a mi mismo
+    });
+});
+
+app.use(express.json());
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
+});
+app.get('/api/historial_chat', async (req, res) => { //listar historial de chat
+    let db = await conectarMongoDb(),
+        collection = db.collection('chats'),
+        chats = await collection.find().sort({ fecha: 1 }).toArray();
+    res.send(chats);
 });
 app.post('/api/amigos', async (req, res) => { //guardar nuevos amigos
     let amigo = req.body, 
@@ -53,7 +72,7 @@ app.delete('/api/amigos/:id', async (req, res) => { //eliminar amigos
         result = await collection.deleteOne({ _id: new ObjectId(id) });
     res.send({msg:result});
 });
-app.listen(port, () => {
+http.listen(port, () => {
     console.log('Servidor corriendo en el puerto ' + port);
 });
 async function conectarMongoDb(){
